7c94b42f30fe70130ad959fd5ded2ea8244847b3
nigue <anibaldavor@gmail.com>
2019-01-09 22:03:58 -0300

    avance

--- src/main/java/com/tapir/org/heroku_sb_test/controller/TestController.java
+++ src/main/java/com/tapir/org/heroku_sb_test/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.tapir.org.heroku_sb_test.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,7 +10,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TestController {
 
     @RequestMapping("/api/helloworld")
-    public String findAll(){
-        return "Hello World!";
+    public ResponseEntity<BasicResponse> findAll(){
+        BasicResponse result = new BasicResponse();
+        result.setCode(1234);
+        result.setMsg("asdfasdfasdf");
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 }
