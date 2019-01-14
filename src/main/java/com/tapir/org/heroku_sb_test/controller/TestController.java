bc21927036c95f8779b64e9b4c089565d4560fa6
mfuenzalida <mfuenzalida@modyo.com>
2019-01-14 11:21:01 -0300

    avance

--- src/main/java/com/tapir/org/heroku_sb_test/controller/TestController.java
+++ src/main/java/com/tapir/org/heroku_sb_test/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.tapir.org.heroku_sb_test.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -9,11 +10,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/tapir")
 public class TestController {
 
+    @Value( "${test.saludo}" )
+    private String saludo;
+
     @RequestMapping("/api/helloworld")
     public ResponseEntity<BasicResponse> findAll(){
         BasicResponse result = new BasicResponse();
         result.setCode(200);
-        result.setMsg("nueva respuesta 3");
+        result.setMsg(saludo);
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 }
